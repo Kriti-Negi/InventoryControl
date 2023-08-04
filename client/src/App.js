5b1080574ee12e2390311859e6a8c9e7c17b5dd4
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-05 00:25:11 +0530

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
-     
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} exact/>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
