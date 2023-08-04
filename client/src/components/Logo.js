e4a3ef57b46c88b5cb986f76a2261826f2d3aa4a
Kriti-Negi <66047981+Kriti-Negi@users.noreply.github.com>
2023-08-04 21:01:49 +0000

    created login and sign up pages

---
+++ client/src/components/Logo.js
@@ -0,0 +1,9 @@
+import logo from '../images/Logo.png'
+
+function Logo(){
+    return (
+        <img src = {logo} alt = "logo"/>
+    )
+}
+
+export default Logo;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Logo from '../components/Logo'
+import Heading from '../components/Heading'
+import SubHeading from '../components/SubHeading'
+import BasicButton from '../components/BasicButton'
+import SignUpLogInTextInput from '../components/SignUpLogInTextInput'
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function updateValues(e){
+    if(e.target.placeholder === "email"){
+      setEmail(e.target.value);
+    }else if(e.target.placeholder === "password"){
+      setPassword(e.target.value);
+    }
+  }
+
+  function logUserIn(){
+    console.log("*backend magic*")
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <div>
+        <Logo/>
+        <Heading text = "Log In."/>
+        <SubHeading text = "Keep Calm. Keep it Going." />
+      </div>
+      <div>
+        <form>
+          <div>
+            <SignUpLogInTextInput type = "text" placeholder = "email" onChange = {(e) => updateValues(e)} value = {email}/>
+            <SignUpLogInTextInput type = "password" placeholder = "password" onChange = {(e) => updateValues(e)} value = {password}/>
+          </div>
+          <p>don't have an account? <Link to = "/signup">Sign Up</Link></p>
+          <BasicButton
+            text = "Log In"
+            onClick = {logUserIn}
+          />
+        </form>
+      </div>
+    </main>
   )
 }
 
