8d27dbec8500740dd877e149a12f2fc218b8e9cb
Kriti-Negi <66047981+Kriti-Negi@users.noreply.github.com>
2023-08-04 21:14:18 +0000

    added html-rect for landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import Logo from '../components/Logo'
+import Heading from '../components/Heading'
+import SubHeading from '../components/SubHeading'
+import BasicButton from '../components/BasicButton'
+
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <main>
+      <div>
+        <Logo/>
+        <Heading text = "Inventory Control"/>
+        <SubHeading text = "Take change of your business. One order at a time."/>
+        <Link to = "/signup">
+          <BasicButton text = "Get Started." onClick = {() => {}}/>
+        </Link>
+      </div>
+    </main>
   )
 }
 
